e0b1308b677b6a8e9bd55a5cd0dbc31a54d7ef74
Amanda Filizola <amandapaivafilizola@gmail.com>
2020-04-08 16:33:01 -0300

    adding header

--- meetup2-app/src/Routes.js
+++ meetup2-app/src/Routes.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { BrowserRouter, Switch ,Route } from 'react-router-dom';
 import Hello from './components/Hello';
 import Goodbye from './components/Goodbye';
+import Header from './components/Header';
 
 export default function Routes() {
   return (
     <BrowserRouter>
+      <Header/>
       <Switch>
-        <Route path="/" exact component={Hello}/>
-        <Route path="/login" component={Goodbye} />
-        <Route path="/login" component={Hello}/>
+        <Route path="/" exact component={Hello} />
+        <Route path="/goodbye" component={Goodbye} />
       </Switch>
-
     </BrowserRouter>
   )
 }
